3572312e1d09bd82a171d16a3c004ed7efcd73cc
Sven Callier <sven.callier@lyceumgent.be>
2024-09-11 22:22:56 +0200

    update

--- exercises/Opdracht_4_buur/solution/solution.py
+++ exercises/Opdracht_4_buur/solution/solution.py
@@ -1,1 +1,6 @@
-print("Hello world")
+naam=input("Hoe heet jij?")
+naamB=input("Hoe heet jouw buur?")
+klas=input("In welke klas zit jij?")
+
+print("Ik ben", naam,"en zit in", klas,"van campus Portus.")
+print(naamB, "zit naast mij tijdens de les informatica.")
