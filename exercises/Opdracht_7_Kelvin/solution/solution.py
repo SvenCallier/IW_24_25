9896d510154732f94db90d7a0b061edf8732fcb7
Sven Callier <sven.callier@lyceumgent.be>
2024-09-12 08:54:55 +0200

    update

--- exercises/Opdracht_7_Kelvin/solution/solution.py
+++ exercises/Opdracht_7_Kelvin/solution/solution.py
@@ -1,4 +1,3 @@
-Nieuw: Sneltoetsen … De sneltoetsen in Drive zijn geüpdatet zodat je nu met de 1e letters kunt navigeren
 in1 = input ("Geef de temperatuur in graden.")
 tempC = float(in1)
 tempK = 273.15 + tempC
